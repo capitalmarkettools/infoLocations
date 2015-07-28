5182200c1faca75e4745430bb0998e8e5ec3ad60
summit <capitalmarkettools.org@gmail.com>
2015-07-28 19:23:10 -0400

    make work for mobile and add meteor directory

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-
 Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
     
     angular.module("infoLoc",['angular-meteor']);
-    
+
+    function onReady() {
+	angular.bootstrap(document, ['infoLoc']);
+    }
+ 
+    if (Meteor.isCordova)
+	angular.element(document).on('deviceready', onReady);
+    else
+	angular.element(document).ready(onReady);
+     
     angular.module("infoLoc").controller("TodosListCtrl", ['$scope', '$meteor', function($scope, $meteor){
 	
 	$scope.tasks = $meteor.collection(Tasks);
